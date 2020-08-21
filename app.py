f8fefdd82561fc5308b1fe3618db89647eb952cc
rsandidge9 <rsandidge9@yahoo.com>
2020-08-20 22:18:40 -0500

    changed app.py file back

--- app.py
+++ app.py
@@ -8,10 +8,5 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/bears")
-def bears():
-    return render_template("bears.html")
-
-
 if __name__ == "__main__":
     app.run(debug=True)
